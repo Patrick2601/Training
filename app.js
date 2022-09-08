db6479d112d267ab41aeca3096b4f9a7ffb22ffa
Pratik Vernekar <pratikvernekar@PratikV-iMac.local>
2022-09-08 14:05:08 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,52 @@
+// function add(n11:number,n22:number){
+//    return (n11+n22)
+// }
+// const n1=5,n2=4;
+// const res=add(n1,n2)
+// console.log(res)
+// {
+//     name:string;
+//     age:number
+// }={
+// :{
+//     name:string;
+//     age:number;
+//     role:[number,string]
+// }
+// enum Role{ADMIN,READ_ONLY,AUTHOR}
+// const emp={
+//     name:"emp1",
+//     age:30,
+//     role:Role.READ_ONLY
+// }
+// //emp.role=[1,'jjj',"random"]
+// emp.role.push(2,'kk')
+// // console.log(emp.name)
+// // console.log(emp.age)
+// console.log(emp.role)
+// console.log(emp)
+// if(emp.role===4){
+//     console.log("ADMIN IN")
+// }
+// function combine(
+//     ip1:number | string,
+//     ip2: number|string,
+//     resultConversion: string
+//     ){
+//     let res
+//     if(typeof ip1==='number' && typeof ip2==='number'){
+//     res=ip1+ip2;
+//     }
+//     else{
+//         res=ip1.toString()+ip2.toString()
+//     }
+//     return res
+// }
+// const combineNum=combine(9,41)
+// console.log(combineNum)
+// const combineStr=combine("44  ","55")
+// console.log(combineStr)
+// console.log("Pratik");
+// const c:number=1;
+// console.log(c)
+// console.warn(Error("jjjj"))
